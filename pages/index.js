d43ab798af5277fdc68b3be0a898840c9e51b3e6
AbdelhakimMhioul <hk.mhl1975@gmail.com>
2022-05-31 15:12:21 +0100

    Feat: Deploy Contracts using Web3

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,52 @@
 import Head from 'next/head';
+import { useState, useEffect } from "react";
+import getWeb3 from "../src/getWeb3";
+// Components
 
 // Smart Contracts' ABIs
 import SupplyChainContract from '../build/contracts/SupplyChain.json';
 
 export default function Home() {
+  // Item State
+  const [item, setItem] = useState({
+    sku: 0,
+    upc: 0,
+    ownerID: '0x0',
+    originFarmerID: '0x0',
+    originFarmName: '',
+    originFarmInformation: '',
+    originFarmLatitude: '',
+    originFarmLongitude: '',
+    productNotes: '',
+    productPrice: 0,
+    distributorID: '0x0',
+    retailerID: '0x0',
+    consumerID: '0x0',
+  });
+  // Web3 State
+  const [web3State, setWeb3State] = useState({
+    web3: null,
+    accounts: null,
+    contract: null
+  });
+
+  useEffect(() => {
+    (async () => {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = SupplyChainContract.networks[networkId];
+      const instance = new web3.eth.Contract(
+        SupplyChainContract.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+      setWeb3State({ web3, accounts, contract: instance })
+    })()
+  }, []);
+
   return (
     <div>
       <Head>

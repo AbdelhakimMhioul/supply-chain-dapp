1c68078094ef47f724c99a12d31d839b0d663557
AbdelhakimMhioul <hk.mhl1975@gmail.com>
2022-05-31 12:46:27 +0100

    Feat: Transform into Progressive Web App (PWA)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')
 const nextConfig = {
   reactStrictMode: true,
+  pwa: {
+    dest: 'public',
+    // disable: process.env.NODE_ENV === 'development',
+  }
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -10,7 +9,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="bg-blue-600">Hello There</div>
+      <div className="bg-blue-600 text-white">Hello There</div>
     </div>
   )
 }
